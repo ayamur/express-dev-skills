a983c9c66db471dfcd9ed3e5978ba1fa8e7102bb
AyaMu <ayalamuruch@gmail.com>
2023-01-04 20:23:41 -0500

    refactor routing. make controller directory. make skills.js within controller directory.

---
+++ data/skill-data.js
@@ -0,0 +1,22 @@
+const skills = [
+  {text: 'HTML', basics: true, _id: 1},
+  {text: 'CSS', basics: true, _id: 2},
+  {text: 'JavaScript', basics: true, _id: 3},
+  {text: 'JSON', basics: false, _id: 4},
+  {text: 'Python', basics: false, _id: 5},
+  {text: 'PostgreSQL', basics: true, _id: 6},
+  {text: 'MongoDB', basics: false, _id: 7},
+  {text: 'Typescript', basics: false, _id: 8},
+  {text: 'EJS', basics: false, _id: 9},
+  {text: 'DTL', basics: false, _id: 10},
+  {text: 'React', basics: false, _id: 11},
+  {text: 'Express.js', basics: true, _id: 12},
+  {text: 'Django', basics: false, _id: 13},
+  {text: 'Jquery', basics: false, _id: 14},
+  {text: 'Bootstrap', basics: false, _id: 15},
+  {text: 'Mongoose', basics: false, _id: },
+]
+
+export { 
+	skills
+}
