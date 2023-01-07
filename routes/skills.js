190eb3d89ee54c3d7935d286946a5bfb78271313
AyaMu <ayalamuruch@gmail.com>
2023-01-07 15:34:45 -0500

    add UI, define route, code and export controller action, and render and write view for new skills input.

--- routes/skills.js
+++ routes/skills.js
@@ -2,8 +2,10 @@ import { Router } from 'express'
 import * as skillsCtrl from '../controllers/skills.js'
 const router = Router()
 
-/* GET users listing. */
+
 router.get('/', skillsCtrl.index)
+router.get('/new', skillsCtrl.new)
+
 export {
   router
 }
