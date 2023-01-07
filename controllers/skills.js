f718145138cb415462c961518363facdcbb628a2
AyaMu <ayalamuruch@gmail.com>
2023-01-07 15:39:27 -0500

    add a function named create and export from controller

--- controllers/skills.js
+++ controllers/skills.js
@@ -17,7 +17,21 @@ function newSkill(req, res) {
   res.render('skills/new')
 }
 
+function create(req, res) {
+  req.body.done = false
+  Todo.create(req.body)
+  .then(skill => {
+		// Notice we are doing a redirect here!
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
 export {
   index,
   newSkill as new,
+  create
 }
